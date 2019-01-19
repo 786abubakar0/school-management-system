a7354f9b1bb36d1d13086d80fa42a4f0557a9649
786abubakar0 <46836643+786abubakar0@users.noreply.github.com>
2019-01-18 23:50:41 -0800

    Initialize

---
+++ MainClass.java
@@ -0,0 +1,19 @@
+public class MainClass
+{
+	public static void main(String[] args)
+	{
+		
+		gui a=new gui();
+		a.display();
+		
+		form b=new form();
+		b.Form();
+		
+	   Table c= new Table();    
+	   c.main();
+	   
+	   searchtable d=new searchtable();
+
+		
+	}
+}
